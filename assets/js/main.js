26646287bb386231f549093c2ecf771e293f78e3
makkunii <mace21218@gmail.com>
2022-10-05 00:08:28 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 	//open nav
+
+	
     function openNav() {
 		document.getElementById("myNav").style.height = "100%";
 		}
@@ -20,6 +22,18 @@
 
 			});
 
+			$(document).ready(function () {
+				$('.move-down').hover(function () {
+					$(this).addClass('fa-angles-up');
+					$(this).removeClass('fa-angles-down'); 
+				}, function () {
+					$(this).addClass('fa-angles-down');
+					$(this).removeClass('fa-angles-up');
+				});
+
+
+			});	
+
 	   //change state of header
 		const navbar = document.querySelector('.header');
 		const logo = document.querySelector('.logo');
@@ -36,4 +50,51 @@
 			}
 		};
 
-        
+		var typed = new Typed('#typed', {
+			stringsElement: '#typed-strings',
+			typeSpeed: 50,
+			backDelay: 700,
+			fadeOutDelay: 500,
+			loop:false,
+			loopCount: Infinity,
+			showCursor: true,
+
+			});
+
+			var pages = ["home","projects","certificates","about"];
+			var pageCounter = 1;
+
+			function scrollDown() {
+			
+			var goTo = document.getElementById(pages[pageCounter]);
+						
+			
+					if (pageCounter < pages.length-1)
+					{
+						pageCounter++;
+						goTo.scrollIntoView({ behavior:"smooth"},true);
+						return;
+					}
+						pageCounter = 0;
+						goTo.scrollIntoView({ behavior:"smooth"},true);
+					
+			}
+
+			function reveal() {
+				var reveals = document.querySelectorAll(".reveal");
+	
+				for (var i = 0; i < reveals.length; i++) {
+					var windowHeight = window.innerHeight;
+					var elementTop = reveals[i].getBoundingClientRect().top;
+					var elementVisible = 150;
+	
+					if (elementTop < windowHeight - elementVisible) {
+					reveals[i].classList.add("active");
+					} else {
+					reveals[i].classList.remove("active");
+					}
+				}
+				}
+	
+			window.addEventListener("scroll", reveal);
+	
