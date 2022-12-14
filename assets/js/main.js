bdae74cc56d32a01284c4f0ccc0765e79f26809c
makkunii <mace21218@gmail.com>
2022-12-14 22:08:27 +0800

    why am i still here

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
     function openNav() {
 		document.getElementById("myNav").style.height = "100%";
 		}
-		//close nav
+		
 		function closeNav() {
 		document.getElementById("myNav").style.height = "0%";
 		}
