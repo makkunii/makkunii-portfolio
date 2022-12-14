0a1978ba30528cb8c5264d780c025a7313070614
makkunii <mace21218@gmail.com>
2022-12-14 21:07:06 +0800

    js img

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,3 +41,83 @@ for (i = 0; i < coll.length; i++) {
 			} 
 			});
 			}
+
+
+    var img1 = document.createElement("img");
+    var img2 = document.createElement("img");
+    var img3 = document.createElement("img");
+    var img4 = document.createElement("img");
+    var img5 = document.createElement("img");
+    var img6 = document.createElement("img");
+    var img7 = document.createElement("img");
+    var img8 = document.createElement("img");
+    var img9 = document.createElement("img");
+    var img10 = document.createElement("img");
+    var img11 = document.createElement("img");
+    var img12 = document.createElement("img");
+    var img13 = document.createElement("img");
+    var img14 = document.createElement("img");
+    var img15 = document.createElement("img");
+
+
+
+
+    
+
+
+
+    img1.src = "/certificates/0001.jpg";
+    img2.src = "/certificates/0002.jpg";
+    img3.src = "/certificates/0003.png";
+    img4.src = "/certificates/0004.jpg";
+    img5.src = "/certificates/0005.jpg";
+    img6.src = "/certificates/0006.jpg";
+    img7.src = "/certificates/0007.jpg";
+    img8.src = "/certificates/0008.jpg";
+    img9.src = "/certificates/0009.jpg";
+    img10.src = "/certificates/0010.jpg";
+    img11.src = "/certificates/0011.jpg";
+    img12.src = "/certificates/0012.jpg";
+    img13.src = "/certificates/0013.png";
+    img14.src = "/certificates/0014.png";
+    img15.src = "/certificates/css-cert.jpg";
+
+
+
+
+    var src1 = document.getElementById("cert1");
+    var src2 = document.getElementById("cert2");
+    var src3 = document.getElementById("cert3");
+    var src4 = document.getElementById("cert4");
+    var src5 = document.getElementById("cert5");
+    var src6 = document.getElementById("cert6");
+    var src7 = document.getElementById("cert7");
+    var src8 = document.getElementById("cert8");
+    var src9 = document.getElementById("cert9");
+    var src10 = document.getElementById("cert10");
+    var src11 = document.getElementById("cert11");
+    var src12 = document.getElementById("cert12");
+    var src13 = document.getElementById("cert13");
+    var src14 = document.getElementById("cert14");
+    var src15 = document.getElementById("cert15");
+
+
+
+
+    src1.appendChild(img1);
+    src2.appendChild(img2);
+    src3.appendChild(img3);
+    src4.appendChild(img4);
+    src5.appendChild(img5);
+    src6.appendChild(img6);
+    src7.appendChild(img7);
+    src8.appendChild(img8);
+    src9.appendChild(img9);
+    src10.appendChild(img10);
+    src11.appendChild(img11);
+    src12.appendChild(img12);
+    src13.appendChild(img13);
+    src14.appendChild(img14);
+    src15.appendChild(img15);
+
+    
